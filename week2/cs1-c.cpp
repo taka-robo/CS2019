1dcf6c85c91255cab14b6b03d2a1d2033d1ad9ad
Takaaki Nara <seeker9895@gmail.com>
2019-10-26 01:16:44 +0900

    ネットからパクってきてそれっぽいのはできた，なんでできるのかはこれからかんがえる

--- week2/cs1-c.cpp
+++ week2/cs1-c.cpp
@@ -8,12 +8,20 @@
 #include <cmath>
 //標準入出力ライブラリ
 #include <iostream>
+//出力整形ライブラリ
+#include <iomanip>
 //STL型ライブラリ
 #include <vector>
 //時間計測用ライブラリ
 #include <chrono>
-
-constexpr int64_t MAX_VAL = 1000000;
+//計算する桁数
+constexpr int8_t Length = 64;
+//配列一要素に格納する桁数
+constexpr int8_t Scale = 8;
+//配列の要素数
+constexpr int32_t ArrayNum = (Length + 7) / Scale;
+//桁をシフトする大きさ
+int32_t Shift = std::pow(10, Scale);
 /**
  * @fn
  * @brief 自然数Nの階乗を計算する関数（64bit以上の計算結果に対応）
@@ -21,31 +29,52 @@ constexpr int64_t MAX_VAL = 1000000;
  * @return なし
  */
 
-void factrial(int64_t N)
+void multi(std::vector<int32_t> &a, int32_t b, std::vector<int32_t> &c)
 {
-  int64_t carry = 0;
-  std::vector<int32_t> n = {100, 000, 000};
-  std::vector<int32_t> temp;
-  temp.resize(n.size());
-
-  //入力されたNを配列nに分割して格納(あとで実装)
-  size_t i;
-  for (i = n.size() - 1; i >= 0; --i)
+  int64_t carry = 0, w;
+  //入力されたnを配列nに分割して格納(あとで実装)
+  int32_t i;
+  for (i = ArrayNum; i >= 0; --i)
   {
-    carry = n[i] * temp[i] + carry;
-    n[i] = carry % 10000;
+    w = a[i];
+    c[i] = (w * b + carry) % Shift;
+    carry = (w * b + carry) / Shift;
+  }
+}
+void show(std::vector<int32_t> &result)
+{
+  int64_t i;
+  for (i = 0; i < ArrayNum; ++i)
+  {
+    printf("%08d,", result[i]);
+  }
+  std::cout << std::endl;
+}
+void factrial(int64_t N, std::vector<int32_t> &result)
+{
+  int32_t i;
+  result[ArrayNum - 1] = 1;
+  for (i = 1; i <= N; ++i)
+  {
+    multi(result, i, result);
+    printf("%4d!=", i);
+    show(result);
   }
 }
 int main()
 {
-  int64_t N;
-  std::cout << "64bitより小さい自然数を入力してください" << std::endl;
+  int32_t N;
+  //計算結果を格納する配列
+  std::vector<int32_t> result(Length, 0);
+  std::cout << "32bitより小さい自然数を入力してください" << std::endl;
+  std::cin >> N;
   //64bit:w以上の値，０以下の値，文字列の入力があった場合の例外処理
-  if (N > std::pow(2, sizeof(int64_t) * 8) || (N <= 0) || std::cin.fail())
+  if (N > std::pow(2, sizeof(int32_t) * 8) || (N <= 0) || std::cin.fail())
   {
     std::cout << "入力値が不正です" << std::endl;
     return -1;
   }
+  factrial(N, result);
 
   return 0;
 }
