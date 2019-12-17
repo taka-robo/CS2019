1c2ebc2c799ff3b8673c3f5717fb987f7e950f4c
Takaaki Nara <seeker9895@gmail.com>
2019-12-17 11:28:06 +0900

    finish 3c

---
+++ week4/3c/list2.cpp
@@ -0,0 +1,237 @@
+#include "list2.hpp"
+
+/**
+ * @brief 指定したポインタの後ろにkeyの値を挿入 
+ * @param key:配列に格納したい変数
+ * @param pt:挿入したい位置の前のノードのポインタ
+ * @return 新しく挿入したノードのアドレス
+ * 　　
+ */
+struct node *insert_after(struct point point, struct node *pt) //挿入したいkeyの値と次に挿入したい場所のポイン
+{
+    struct node *new_node;                              //挿入されるキーを格納するノードへのポインタ
+    new_node = (struct node *)malloc(sizeof *new_node); //ノードの確保
+    if (new_node == NULL)
+    { //確保できなかった場合は
+        printf("Not enough memory\n");
+        exit(1); //終了
+    }
+    new_node->cell.col = point.col; //新しいノードのxに値を格納
+    new_node->cell.row = point.row; //新しいノードのyに値を格納
+    new_node->next = pt->next;      //new_nodeのnextはptのnext
+    pt->next = new_node;            //ptのnextはnew_node
+    return new_node;                //new_nodeの先頭アドレスを返す
+}
+/**
+ * @brief 指定したリストの中身をすべて表示する
+ * @param *pt:表示したいリストの先頭のアドレス
+ */
+void print_whole_list(struct node *pt)
+{
+    for (pt = pt->next; pt != NULL; pt = pt->next)
+    {
+        printf("%d,%d -> ", pt->cell.col, pt->cell.row); //pのkeyを表示して
+    }
+    printf("\n");
+}
+/**
+ * @brief listの全ノードを削除する
+ * @return なし
+ */
+void delete_all(struct node *head)
+{
+    struct node *p;
+    struct node *temp;
+    //pの値がNULLになるまで繰り返す
+    for (p = head->next; p != NULL; p = p->next)
+    {
+        //headの次の次のノードアドレスを一時変数に記憶しておく
+        temp = p->next;
+        //headの次のノードのメモリを開放する
+        free(head->next);
+        //headの次のノードを記憶しておいた次の次のノードのアドレスにつなぎ直す．
+        head->next = temp;
+    }
+}
+/**
+ * @brief スタックリストの初期化
+ * @param *head:スタックリストに使うリストの先頭のアドレス 
+ * @return なし
+ */
+void stackinit(struct node *head)
+{
+    std::cout << __func__ << std::endl;
+    delete_all(head);
+    head->next = NULL;
+}
+/**
+ * @brief スタックに整数numを入れる
+ * @param *head:スタックリストに使うリストの先頭のアドレス 
+ * @param  num:スタックに入れたい整数 
+ * @return なし
+ */
+void push(struct node *head, int col, int row)
+{
+    std::cout << __func__ << ":" << col << "," << row << "   ";
+    struct node *p;
+    struct point pt;
+    pt.col = col;
+    pt.row = row;
+    for (p = head; p->next != NULL; p = p->next)
+    {
+        //リストの末尾までnextを辿っていく処理（効率が悪い）
+    }
+    //リストの末尾の次にnumを挿入
+    insert_after(pt, p);
+    print_whole_list(head);
+}
+/**
+ * @brief スタックから整数を一つとりだす
+ * @param *head:スタックリストに使うリストの先頭のアドレス 
+ * @return 取り出した整数
+ */
+struct point pop(struct node *head)
+{
+    std::cout << __func__;
+    struct point temp;
+    struct node *p;
+    if (stackempty(head) == 0)
+    {
+        for (p = head; p->next->next != NULL; p = p->next)
+        {
+            //リストの末尾の一個前までリストを辿っていく処理（効率が悪い）
+        }
+    }
+    else
+    {
+        std::cout << " stack is empty" << std::endl;
+        //空のスタックからpopしようとしたのでプログラムを終了
+        exit(1);
+    }
+    //pの次の要素の値を取得し削除
+    temp = delete_next(p);
+    std::cout << ":" << temp.col << "," << temp.row << "   ";
+    print_whole_list(head);
+    return temp;
+}
+/**
+ * @brief スタックが空だったら非0の整数を返し，中身があったら0を返す
+ * @param *head:スタックリストに使うリストの先頭のアドレス 
+ * @return 非0:スタックが空
+ *           0:スタックに中身がある
+ */
+int stackempty(struct node *head)
+{
+    //headの後ろに要素が存在すれば0を返す，なければ非0を返す
+    if (head->next != NULL)
+    {
+        //中身が存在するので0を返す
+        return 0;
+    }
+    else
+    {
+        //中身が存在しないので非0を返す
+        return 1;
+    }
+}
+/**
+ * @brief キューリストの初期化
+ * @param *head:キューリストに使うリストの先頭のアドレス 
+ * @return なし
+ */
+void queueinit(struct node *head)
+{
+    std::cout << __func__ << std::endl;
+    head->next = NULL;
+}
+/**
+ * @brief キューに整数numを入れる
+ * @param *head:キューリストに使うリストの先頭のアドレス 
+ * @param  num:キューに入れたい整数 
+ * @return なし
+ */
+void put(struct node *head, int col, int row)
+{
+    struct point pt;
+    pt.col = col;
+    pt.row = row;
+    std::cout << __func__ << ":" << col << "," << row << "   ";
+    struct node *p;
+    for (p = head; p->next != NULL; p = p->next)
+    {
+        //リストの末尾までnextを辿っていく処理（効率が悪い）
+    }
+
+    insert_after(pt, p);
+    print_whole_list(head);
+}
+/**
+ * @brief キューから整数を一つとりだす
+ * @param *head:スタックリストに使うリストの先頭のアドレス 
+ * @return 取り出した整数
+ */
+struct point get(struct node *head)
+{
+    std::cout << __func__;
+    struct point temp;
+    if (queueempty(head) == 0)
+    {
+        //リストの一番先頭のノードのkeyを取得して先頭のノードを削除
+        temp = delete_next(head);
+        std::cout << ":" << temp.col << "," << temp.row << "   ";
+        print_whole_list(head);
+        return temp;
+    }
+    else
+    {
+        std::cout << " queue is empty" << std::endl;
+        //空のキューからゲットしようとしたのでプログラムを終了
+        exit(1);
+    }
+}
+/**
+ * @brief キューが空だったら非0の整数を返し，中身があったら0を返す
+ * @param *head:キューリストに使うリストの先頭のアドレス 
+ * @return 非0:キューが空
+ *           0:キューに中身がある
+ */
+int queueempty(struct node *head)
+{
+    //headの後ろに要素が存在すれば0を返す，なければ非0を返す
+    if (head->next != NULL)
+    {
+        //要素が存在するので0を返す
+        return 0;
+    }
+    else
+    {
+        //要素が存在しないので非0を返す
+        return 1;
+    }
+}
+/**
+ * @brief 指定場所の直後の要素のkeyを返すと共にその要素を削除する
+ * @param *pt:keyを取得し消去したいノードの前のノードのアドレス 
+ * @return　指定場所の直後のノードのkey
+ */
+struct point delete_next(struct node *pt)
+{
+    struct point point;
+    //ptの次にノードがなければエラーとしてプログラムを終了
+    if (NULL == pt->next)
+    {
+        std::cout << "delete_next error" << std::endl;
+        exit(1);
+    }
+    //指定箇所の次のノードのx,yを格納
+    point.col = pt->next->cell.col;
+    point.row = pt->next->cell.row;
+    //指定箇所の次のノードの次のノードのアドレスを格納
+    struct node *temp = pt->next->next;
+    //指定箇所の次のノードのメモリを開放
+    free(pt->next);
+    //指定箇所の次のノードを削除したノードの次のノードにつなぎ替える
+    pt->next = temp;
+    //削除したノードのkeyを返す
+    return point;
+}
---
+++ week4/3c/list2.hpp
@@ -0,0 +1,33 @@
+#ifndef INCLUDE_LIST2_H
+#define INCLUDE_LIST2_H
+
+#include <iostream>
+
+struct point
+{
+    int col = 0;
+    int row = 0;
+};
+struct node
+{
+    struct point cell;
+    struct node *next; //次のノードのアドレス
+};
+
+// void list_initialize(struct node *head);
+struct node *insert_after(struct point point, struct node *pt);
+void print_whole_list(struct node *pt);
+void delete_all(struct node *head);
+
+void stackinit(struct node *head);
+void push(struct node *head, int col, int row);
+struct point pop(struct node *head);
+int stackempty(struct node *head);
+
+void queueinit(struct node *head);
+void put(struct node *head, int col, int row);
+struct point get(struct node *head);
+int queueempty(struct node *head);
+
+struct point delete_next(struct node *pt);
+#endif
